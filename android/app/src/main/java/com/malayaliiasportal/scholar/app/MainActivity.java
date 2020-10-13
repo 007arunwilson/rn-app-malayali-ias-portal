0f8950f0534140e13dc3698660e102e253f390af
Arun Wilson <007arunwilson@gmail.com>
2020-10-13 17:36:38 +0530

    screen shot is disabled

--- android/app/src/main/java/com/malayaliiasportal/scholar/app/MainActivity.java
+++ android/app/src/main/java/com/malayaliiasportal/scholar/app/MainActivity.java
@@ -1,8 +1,14 @@
 package com.malayaliiasportal.scholar.app;
-
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
   
 }
